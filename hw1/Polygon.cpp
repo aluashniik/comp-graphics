be1625a03a69b31da10669243549e479abcb995e
aluashniik <144539773+aluashniik@users.noreply.github.com>
2025-06-29 18:16:05 +0900

    add

---
+++ hw1/Polygon.cpp
@@ -0,0 +1,81 @@
+﻿#include <stdio.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+#include "Definitions.h"
+
+float line_color[3] = { 0.0f, 0.0f, 1.0f }; // 초기값: 파랑
+//int center_selected = 0; // 중심점이 클릭되었는지 확인
+
+void add_point(My_Polygon *pg, Window *wd, int x, int y) {
+	pg->point[pg->n_points][0] = 2.0f * ((float)x) / wd->width - 1.0f;
+	pg->point[pg->n_points][1] = 2.0f * ((float)wd->height - y) / wd->height - 1.0f;
+	pg->n_points++; 
+}
+
+void close_line_segments(My_Polygon *pg) {
+	pg->point[pg->n_points][0] = pg->point[0][0];
+	pg->point[pg->n_points][1] = pg->point[0][1];
+	pg->n_points++;
+}
+
+void draw_lines_by_points(My_Polygon* pg, int polygon_mode) {
+	/*glColor3f(CENTER_POINT_COLOR);
+	glBegin(GL_POINTS);
+	glVertex2f(pg->center_x, pg->center_y);
+	glEnd();*/
+
+	if (polygon_mode) {
+		glColor3f(CENTER_POINT_COLOR);
+		glBegin(GL_POINTS);
+		glVertex2f(pg->center_x, pg->center_y);
+		glEnd();
+	}
+
+	glColor3f(POINT_COLOR);
+	for (int i = 0; i < pg->n_points; i++) {
+		glBegin(GL_POINTS);
+		glVertex2f(pg->point[i][0], pg->point[i][1]);
+		glEnd();
+	}
+	glColor3f(line_color[0], line_color[1], line_color[2]);
+	//glColor3f(LINE_COLOR);
+	glBegin(GL_LINE_LOOP);
+	for (int i = 0; i < pg->n_points; i++)  
+		glVertex2f(pg->point[i][0], pg->point[i][1]);
+	glEnd();
+}
+
+
+void update_center_of_gravity(My_Polygon* pg) {
+	pg->center_x = pg->center_y = 0.0f;
+	if (pg->n_points == 0) return;
+	for (int i = 0; i < pg->n_points; i++) {
+		pg->center_x += pg->point[i][0], pg->center_y += pg->point[i][1];
+	}
+	pg->center_x /= (float)pg->n_points, pg->center_y /= (float)pg->n_points;
+}
+
+void move_points(My_Polygon* pg, float del_x, float del_y) {
+	for (int i = 0; i < pg->n_points; i++) {
+		pg->point[i][0] += del_x, pg->point[i][1] += del_y;
+	}
+}
+
+//이동-회전-이동..
+void rotate_points_around_center_of_grivity(My_Polygon* pg) {
+	for (int i = 0; i < pg->n_points; i++) {
+		float x, y;
+		x = COS_5_DEGREES * (pg->point[i][0] - pg->center_x)
+			- SIN_5_DEGREES * (pg->point[i][1] - pg->center_y) + pg->center_x;
+		y = SIN_5_DEGREES * (pg->point[i][0] - pg->center_x)
+			+ COS_5_DEGREES * (pg->point[i][1] - pg->center_y) + pg->center_y;
+		pg->point[i][0] = x, pg->point[i][1] = y;
+	}
+}
+void scaling_polygon(My_Polygon* pg, float scale_factor) {
+	for (int i = 0; i < pg->n_points; i++) {
+		// 무게중심을 기준으로 스케일링
+		pg->point[i][0] = pg->center_x + (pg->point[i][0] - pg->center_x) * scale_factor;
+		pg->point[i][1] = pg->center_y + (pg->point[i][1] - pg->center_y) * scale_factor;
+	}
+}
